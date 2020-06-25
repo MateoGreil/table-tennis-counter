317af278383f05447c157c5b01c0d0bcd5a65b00
mat <mateo.greil@gmail.com>
2020-06-26 01:42:39 +0200

    .

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ApplicationRecord
   belongs_to :first_team, class_name: 'Team'
   belongs_to :second_team, class_name: 'Team'
+  belongs_to :match, optional: true
 end
--- db/migrate/20200625215256_create_games.rb
+++ db/migrate/20200625215256_create_games.rb
@@ -3,6 +3,7 @@ class CreateGames < ActiveRecord::Migration[6.0]
     create_table :games do |t|
       t.references :first_team, null: false, foreign_key: { to_table: :teams }
       t.references :second_team, null: false, foreign_key: { to_table: :teams }
+      t.references :match, foreign_key: true
 
       t.timestamps
     end
