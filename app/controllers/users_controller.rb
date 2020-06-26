9a9c0f9a2ff96fdf3fa6af98cae2ed479aca720a
mat <mateo.greil@gmail.com>
2020-06-26 13:09:08 +0200

    add order by to users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,28 @@
 class UsersController < InheritedResources::Base
   actions :all, except: :index
 
+  PARAMS_ORDER_BY = %w[login games_w games_l games_w_l matches_w matches_l
+                       matches_w_l total_points average_points].freeze
+  PARAMS_DIRECTION = %w[ASC DESC].freeze
+
   def index
+    order_by = params[:order_by] if params[:order_by].in?(PARAMS_ORDER_BY)
+    direction = if params[:direction].in?(PARAMS_DIRECTION)
+                  params[:direction]
+                else
+                  ''
+                end
     @collection = ActiveRecord::Base.connection.execute(
       <<-SQL
-        SELECT users.login,
-          COUNT(teams.id) AS teams,
+        SELECT login,
           COUNT(games_w) AS games_w,
           COUNT(games_l) AS games_l,
           round(COUNT(games_w)::decimal / (NULLIF(COUNT(games_l), 0)), 3) AS games_w_l,
           COUNT(DISTINCT matches_w) AS matches_w,
           COUNT(DISTINCT matches_l) AS matches_l,
           round(COUNT(DISTINCT matches_w)::decimal / (NULLIF(COUNT(DISTINCT matches_l), 0)), 3) AS matches_w_l,
-          (SUM(games_f.first_team_points) + SUM(games_s.second_team_points)) AS total_team_points
+          SUM(COALESCE(games_f.first_team_points, 0) + COALESCE(games_s.second_team_points, 0)) AS total_points,
+          round(AVG(COALESCE(games_f.first_team_points, 0) + COALESCE(games_s.second_team_points, 0)), 3) AS average_points
         FROM users
         LEFT OUTER JOIN team_users ON team_users.user_id = users.id
         LEFT OUTER JOIN teams ON team_users.team_id = teams.id
@@ -32,7 +42,8 @@ class UsersController < InheritedResources::Base
           ON games_f.first_team_id = teams.id
         LEFT OUTER JOIN games AS games_s
           ON games_s.second_team_id = teams.id
-        GROUP BY users.id;
+        GROUP BY users.id
+        #{order_by ? 'ORDER BY ' + order_by + ' ' + direction : ''};
       SQL
     )
   end
