1b8c72301e3f7141e62a1e6c16f587fc860b2fb6
Mateo Greil <mateo.greil@gmail.com>
2020-08-17 21:55:22 +0200

    add 401

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    render :file => "#{Rails.root}/public/401.html", :status => 401, :layout => false
+  end
 end
